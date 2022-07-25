2a79f6abb7c240192357ec962573bcce733daba3
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-07-25 10:02:35 +0200

    feat: add Vehicles component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { vehicleList } from "./Vehicles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function normalizeVehicles(array) {
+  const normalized = [];
+  array.forEach((element) => {
+    if (Array.isArray(element)) {
+      normalized.push(...element);
+    } else {
+      normalized.push(element);
+    }
+  });
+  return normalized;
 }
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    vehicles: [],
+  };
+
+  componentDidMount() {
+    this.setState({
+      vehicles: normalizeVehicles(vehicleList),
+    });
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
